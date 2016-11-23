0a72077cd0c75d3ea1555929f18c21ed77e5c1ea
gwizdala <gwizdala@vt.edu>
2016-11-23 12:37:31 -0500

    Updating QOTD and testing Cross-origin requests

--- js/iosClaire.js
+++ js/iosClaire.js
@@ -2,22 +2,31 @@
 //  Functions for buttons of iOSClaire Application
 //
 
-function loveQuoteOTD()
+//when we start up we want to run the quote of the Day
+$( document ).ready(function() {
+  quoteOTD();
+});
+
+function quoteOTD()
 {
   $.ajax( {
-        url: 'http://quotes.rest/qod.json',
+        url: 'http://quotesondesign.com/api/3.0/api-3.0.json',
+        crossDomain: true,
+        dataType: 'jsonp',
+        type: 'GET',
         success: function(data) {
           $('#quote-title').text("Quote of the Day");
-          $('#quote-content').html(data.contents.quotes[0].quote);
-          $('#quote-author').html(data.contents.quotes[0].author);
+          $('#quote-content').html(data.quote);
+          $('#quote-author').html(data.author);
         },
         cache: false
   });
 }
-function quoteOTD()
+
+function loveQuoteOTD()
 {
   $.ajax( {
-        url: 'http://quotes.rest/qod.json?category=love',
+        url: 'http://quotes.rest/qod.json',
         success: function(data) {
           $('#quote-title').text("Love Quote of the Day");
           $('#quote-content').html(data.contents.quotes[0].quote);
@@ -26,10 +35,14 @@ function quoteOTD()
         cache: false
   });
 }
+
 function randomQuote()
 {
-  $.ajax( {
+  $.getJSON( {
         url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en',
+        crossDomain: true,
+        dataType: 'jsonp',
+        type: 'GET',
         success: function(data) {
           console.log(data);
           $('#quote-title').text("Random Quote");
