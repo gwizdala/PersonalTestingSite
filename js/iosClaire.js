4787fc01abf9123359171b72e86c4e8533ab0348
gwizdala <gwizdala@vt.edu>
2016-11-23 19:16:06 -0500

    Using AJAX to pull RSS feeds for daily quotes

--- js/iosClaire.js
+++ js/iosClaire.js
@@ -9,15 +9,21 @@ $( document ).ready(function() {
 
 function quoteOTD()
 {
+  var url = "http://feeds.feedburner.com/brainyquote/QUOTEBR";
   $.ajax( {
-        url: 'http://quotesondesign.com/api/3.0/api-3.0.json',
+        url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
         crossDomain: true,
-        dataType: 'jsonp',
-        type: 'GET',
+        dataType: 'json',
         success: function(data) {
+          var result = data.responseData.feed;
           $('#quote-title').text("Quote of the Day");
-          $('#quote-content').html(data.quote);
-          $('#quote-author').html(data.author);
+          $('#quote-content').html(result.entries[0].content);
+
+          if(result.entries[0].author != '') {
+            $('#quote-author').html(result.entries[0].author);
+          } else {
+            $('#quote-author').html("Unknown");
+          }
         },
         cache: false
   });
@@ -25,12 +31,21 @@ function quoteOTD()
 
 function loveQuoteOTD()
 {
+  var url = "http://feeds.feedburner.com/brainyquote/QUOTELO";
   $.ajax( {
-        url: 'http://quotes.rest/qod.json',
+        url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+        crossDomain: true,
+        dataType: 'json',
         success: function(data) {
+          var result = data.responseData.feed;
           $('#quote-title').text("Love Quote of the Day");
-          $('#quote-content').html(data.contents.quotes[0].quote);
-          $('#quote-author').html(data.contents.quotes[0].author);
+          $('#quote-content').html(result.entries[0].content);
+
+          if(result.entries[0].author != '') {
+            $('#quote-author').html(result.entries[0].author);
+          } else {
+            $('#quote-author').html("Unknown");
+          }
         },
         cache: false
   });
@@ -38,14 +53,24 @@ function loveQuoteOTD()
 
 function randomQuote()
 {
-  $.getJSON( {
-        url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en',
-        crossDomain: true,
+  $.ajax({
+        url: "http://api.forismatic.com/api/1.0/",
+        jsonp: "jsonp",
+        dataType: "jsonp",
+        data: {
+          method: "getQuote",
+          lang: "en",
+          format: "jsonp"
+        },
         success: function(data) {
-          console.log(data);
           $('#quote-title').text("Random Quote");
-          $('#quote-content').html(data.quoteText);
-          $('#quote-author').html(data.quoteAuthor);
+          $('#quote-content').html('"' + data.quoteText + '"');
+
+          if(data.quoteAuthor != '') {
+            $('#quote-author').html(data.quoteAuthor);
+          } else {
+            $('#quote-author').html("Unknown");
+          }
         },
         cache: false
   });
